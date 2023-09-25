30521a7f36693b7ae6065d5682f82ac287bcc5e6
coob <koosanghwi@gmail.com>
2023-09-25 14:26:01 +0900

    ver 1.31

--- data_moon/mymodule/action_moon.py
+++ data_moon/mymodule/action_moon.py
@@ -136,7 +136,8 @@ def clean_screen(cla):
     import numpy as np
     import cv2
     from function_moon import imgs_set_, click_pos_reg, click_pos_2
-    from schedule import myQuest_play_add
+    from schedule import myQuest_play_check
+    from dead import dead_die
 
     try:
         print("clean_screen")
@@ -159,6 +160,20 @@ def clean_screen(cla):
             if imgs_ is not None and imgs_ != False:
                 click_pos_reg(imgs_.x, imgs_.y, cla)
 
+            # 죽었는지...
+            full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\dead\\boohwal_1.PNG"
+            img_array = np.fromfile(full_path, np.uint8)
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            imgs_ = imgs_set_(570, 870, 700, 950, cla, img, 0.8)
+            if imgs_ is not None and imgs_ != False:
+                result_schedule = myQuest_play_check(v_.now_cla, "check")
+                print("confirm_all : result_schedule", result_schedule)
+                character_id = result_schedule[0][1]
+                sche = result_schedule[0][2]
+
+                dead_die(cla, sche)
+
+            # 각종 창 닫기
             have_x = False
 
             full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\clean_screen\\x_1.PNG"
