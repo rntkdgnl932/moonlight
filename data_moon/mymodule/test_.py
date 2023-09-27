2cdb255c506259fa0f849af0a028dabe9f5e2db0
coob <koosanghwi@gmail.com>
2023-09-27 22:19:41 +0900

    ver 1.364

--- data_moon/mymodule/test_.py
+++ data_moon/mymodule/test_.py
@@ -40,7 +40,7 @@ def go_test():
     full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\dead\\dead_nabi.PNG"
     img_array = np.fromfile(full_path, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    imgs_ = imgs_set_(300, 280, 650, 450, cla, img, 0.8)
+    imgs_ = imgs_set_(300, 600, 650, 800, cla, img, 0.8)
     if imgs_ is not None and imgs_ != False:
         print("good", imgs_)
     else:
