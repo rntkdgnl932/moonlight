2b9ec931c9744863609fa8b90abef5cda88a475e
coob <koosanghwi@gmail.com>
2023-10-01 20:21:40 +0900

    ver 1.433

--- data_moon/mymodule/action_moon.py
+++ data_moon/mymodule/action_moon.py
@@ -303,7 +303,7 @@ def menu_open(cla):
                 full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\clean_screen\\x_1.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(30, 30, 960, 1030, cla, img, 0.8)
+                imgs_ = imgs_set_(30, 300, 960, 1030, cla, img, 0.8)
                 if imgs_ is not None and imgs_ != False:
                     print("x_1", imgs_)
                     click_pos_reg(imgs_.x, imgs_.y, cla)
@@ -311,7 +311,7 @@ def menu_open(cla):
                     full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\clean_screen\\x_2.PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(30, 30, 960, 1030, cla, img, 0.8)
+                    imgs_ = imgs_set_(30, 300, 960, 1030, cla, img, 0.8)
                     if imgs_ is not None and imgs_ != False:
                         print("x_2", imgs_)
                         click_pos_reg(imgs_.x, imgs_.y, cla)
