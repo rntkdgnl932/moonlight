e9ecffd884f5868d3998c3126e5779a00292c507
coob <koosanghwi@gmail.com>
2023-10-01 20:13:58 +0900

    ver 1.432

--- data_moon/mymodule/action_moon.py
+++ data_moon/mymodule/action_moon.py
@@ -292,6 +292,25 @@ def menu_open(cla):
     try:
         print("menu_open")
 
+        # 경험치 회복창 끄기
+        full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\dead\\exp_recovery_title.PNG"
+        img_array = np.fromfile(full_path, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        imgs_ = imgs_set_(400, 320, 540, 380, cla, img, 0.8)
+        if imgs_ is not None and imgs_ != False:
+            print("menu_open : exp recovery title", imgs_)
+            for i in range(5):
+                full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\clean_screen\\x_1.PNG"
+                img_array = np.fromfile(full_path, np.uint8)
+                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                imgs_ = imgs_set_(30, 30, 960, 1030, cla, img, 0.8)
+                if imgs_ is not None and imgs_ != False:
+                    click_pos_reg(imgs_.x, imgs_.y, cla)
+                else:
+                    break
+                time.sleep(0.3)
+
+
         opened = False
         opened_count = 0
         while opened is False:
