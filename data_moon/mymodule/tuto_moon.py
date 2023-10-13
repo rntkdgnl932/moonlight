b029ebf1e21321fb9c2fb272ec58009e8502ba6d
coob <koosanghwi@gmail.com>
2023-10-14 04:23:46 +0900

    ver 2.36

--- data_moon/mymodule/tuto_moon.py
+++ data_moon/mymodule/tuto_moon.py
@@ -175,6 +175,13 @@ def tuto_click(cla):
     try:
         print("tuto_click", cla)
 
+        full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\action\\menu_character_select.PNG"
+        img_array = np.fromfile(full_path, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        imgs_ = imgs_set_(670, 470, 960, 670, cla, img, 0.8)
+        if imgs_ is not None and imgs_ != False:
+            click_pos_2(935, 55, cla)
+            time.sleep(0.1)
 
         result_out = out_check(cla)
         if result_out == False:
