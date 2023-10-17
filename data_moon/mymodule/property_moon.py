02da38eca63dcbba6370e53f0df3771a82398868
coob <koosanghwi@gmail.com>
2023-10-17 19:39:40 +0900

    ver 2.52

--- data_moon/mymodule/property_moon.py
+++ data_moon/mymodule/property_moon.py
@@ -70,21 +70,21 @@ def my_property_upload(cla):
         # result_mine[0] => 골드
         # result_mine[1] => 다이아
 
-        # # 파악한 곳 나가기
-        for i in range(10):
-            full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\title\\budy_title.PNG"
-            img_array = np.fromfile(full_path, np.uint8)
-            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-            imgs_ = imgs_set_(5, 30, 150, 80, cla, img, 0.8)
-            if imgs_ is not None and imgs_ != False:
-                click_pos_2(935, 55, cla)
-            else:
-                result_out = out_check(cla)
-                if result_out == True:
-                    break
-                else:
-                    clean_screen(cla)
-            time.sleep(0.5)
+        # # # 파악한 곳 나가기
+        # for i in range(10):
+        #     full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\title\\budy_title.PNG"
+        #     img_array = np.fromfile(full_path, np.uint8)
+        #     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        #     imgs_ = imgs_set_(5, 30, 150, 80, cla, img, 0.8)
+        #     if imgs_ is not None and imgs_ != False:
+        #         click_pos_2(935, 55, cla)
+        #     else:
+        #         result_out = out_check(cla)
+        #         if result_out == True:
+        #             break
+        #         else:
+        #             clean_screen(cla)
+        #     time.sleep(0.5)
 
         if int(result_mine[1]) != 0:
 
