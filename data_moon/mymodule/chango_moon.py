b1cefb34cd6056881c3adf422fd7e6d21542cb8b
coob <koosanghwi@gmail.com>
2023-10-26 01:10:08 +0900

    ver 2.82

--- data_moon/mymodule/chango_moon.py
+++ data_moon/mymodule/chango_moon.py
@@ -270,7 +270,7 @@ def chango_jaelyo_in_start(cla):
                     full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\chango\\bogwanhagi_right.PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(820, 920, 910, 970, cla, img, 0.8)
+                    imgs_ = imgs_set_(820, 920, 910, 1040, cla, img, 0.8)
                     if imgs_ is not None and imgs_ != False:
                         click_pos_reg(imgs_.x, imgs_.y, cla)
                         time.sleep(0.4)
@@ -328,7 +328,7 @@ def chango_jaelyo_in_start(cla):
                 full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\chango\\bogwanhagi_right.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(820, 920, 910, 970, cla, img, 0.8)
+                imgs_ = imgs_set_(820, 920, 910, 1040, cla, img, 0.8)
                 if imgs_ is not None and imgs_ != False:
                     click_pos_reg(imgs_.x, imgs_.y, cla)
                     time.sleep(0.1)
@@ -380,7 +380,7 @@ def chango_jaelyo_in_start(cla):
                     full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\chango\\bogwanhagi_right.PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(820, 920, 910, 970, cla, img, 0.8)
+                    imgs_ = imgs_set_(820, 920, 910, 1040, cla, img, 0.8)
                     if imgs_ is not None and imgs_ != False:
                         click_pos_reg(imgs_.x, imgs_.y, cla)
                         time.sleep(0.4)
@@ -499,7 +499,7 @@ def chango_jangbi_in_start(cla):
                         full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\chango\\bogwanhagi_right.PNG"
                         img_array = np.fromfile(full_path, np.uint8)
                         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                        imgs_ = imgs_set_(820, 920, 910, 970, cla, img, 0.8)
+                        imgs_ = imgs_set_(820, 920, 910, 1040, cla, img, 0.8)
                         if imgs_ is not None and imgs_ != False:
                             click_pos_reg(imgs_.x, imgs_.y, cla)
                             time.sleep(0.1)
@@ -526,7 +526,7 @@ def chango_jangbi_in_start(cla):
                         full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\chango\\bogwanhagi_right.PNG"
                         img_array = np.fromfile(full_path, np.uint8)
                         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                        imgs_ = imgs_set_(820, 920, 910, 970, cla, img, 0.8)
+                        imgs_ = imgs_set_(820, 920, 910, 1040, cla, img, 0.8)
                         if imgs_ is not None and imgs_ != False:
                             click_pos_reg(imgs_.x, imgs_.y, cla)
                             time.sleep(0.1)
@@ -900,7 +900,7 @@ def chango_auction_in_start(cla):
             full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\chango\\bogwanhagi_right.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-            imgs_ = imgs_set_(820, 920, 910, 970, cla, img, 0.8)
+            imgs_ = imgs_set_(820, 920, 910, 1040, cla, img, 0.8)
             if imgs_ is not None and imgs_ != False:
                 click_pos_reg(imgs_.x, imgs_.y, cla)
                 time.sleep(0.1)
@@ -1152,7 +1152,7 @@ def dajoong_click(cla, data):
         full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\chango\\checked_off.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(480, 340, 530, 390, cla, img, 0.85)
+        imgs_ = imgs_set_(480, 340, 530, 390, cla, img, 0.8)
         if imgs_ is not None and imgs_ != False:
             print("checked_off", imgs_)
             click_pos_reg(imgs_.x, imgs_.y, cla)
