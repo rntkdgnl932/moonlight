24f460439ace7e1225619573ac6583f1c715ab15
coob <koosanghwi@gmail.com>
2023-12-01 02:01:00 +0900

    ver 3.31

--- data_moon/mymodule/jadong_moon.py
+++ data_moon/mymodule/jadong_moon.py
@@ -52,7 +52,7 @@ def jadong_start(cla, sche):
         full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\jadong\\map_list\\" + result_data[3] + ".PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(50, 110, 160, 160, cla, img, 0.8)
+        imgs_ = imgs_set_(50, 110, 160, 160, cla, img, 0.75)
         if imgs_ is not None and imgs_ != False:
 
             full_path = "c:\\my_games\\moonlight\\data_moon\\imgs\\maul\\chango.PNG"
